3576bbeb2474d7d4a3d6a721ade93b24c04c1458
ceyang1 <30599504+ceyang1@users.noreply.github.com>
2017-08-03 16:36:45 -0400

    Update MERGE_STD_BC.F90

--- MERGE_STD_BC.F90
+++ MERGE_STD_BC.F90
@@ -21,20 +21,147 @@ INCLUDE SUBST_VGRD_ID
 INCLUDE SUBST_COORD_ID
 INCLUDE SUBST_CMAQ_ID
 
-REAL*4,   INTENT(IN)  :: IN_DATA( 2*NTHIK*( NCOLS + NROWS + 2*NTHIK ), NLAYS, NVARS)
-REAL*4,   INTENT(OUT) :: OUT_DATA( 2*NTHIK*( NCOLS + NROWS + 2*NTHIK ), NLAYS, NVARS_BUFF)
+REAL (kind=4), INTENT(IN)  :: IN_DATA( 2*NTHIK*( NCOLS + NROWS + 2*NTHIK ), NLAYS, NVARS)
+REAL (kind=4), INTENT(OUT) :: OUT_DATA( 2*NTHIK*( NCOLS + NROWS + 2*NTHIK ), NLAYS, NVARS_BUFF)
 INTEGER,  INTENT(OUT) :: NVARS_LEFT
-CHARACTER*16, INTENT(OUT) :: OUT_NAME(NVARS_BUFF),  OUT_UNITS(NVARS_BUFF)
-CHARACTER*80, INTENT(OUT) :: OUT_DES(NVARS_BUFF)
+CHARACTER (LEN=16), INTENT(OUT) :: OUT_NAME(NVARS_BUFF),  OUT_UNITS(NVARS_BUFF)
+CHARACTER (LEN=80), INTENT(OUT) :: OUT_DES(NVARS_BUFF)
 
-CHARACTER*16, ALLOCATABLE :: standard_vname(:), standard_units(:)
-CHARACTER*80, ALLOCATABLE :: standard_vdesc(:)
+CHARACTER (LEN=16), ALLOCATABLE :: standard_vname(:), standard_units(:)
+CHARACTER (LEN=80), ALLOCATABLE :: standard_vdesc(:)
 
-CHARACTER*16, ALLOCATABLE :: geos_vname(:), geos_units(:), left_vname(:), left_units(:)
-CHARACTER*80, ALLOCATABLE :: geos_vdesc(:), left_vdesc(:)
+CHARACTER (LEN=16), ALLOCATABLE :: geos_vname(:), geos_units(:), left_vname(:), left_units(:)
+CHARACTER (LEN=80), ALLOCATABLE :: geos_vdesc(:), left_vdesc(:)
 
 INTEGER :: extra_nvar, left_nvar
-INTEGER :: stat, nbndy, n, v, i, standard_jdate, standard_jtime, geos_jdate, geos_jtime,   &
+INTEGER :: stat, nbndy, n, v, i, standard_jdate, standard_jtime, geos_jdate, geos_jtime,     &
            standard_nvar, geos_nvar, total_nvar, standard_nsteps, geos_nsteps, geos_tsteps
 
 INTEGER, ALLOCATABLE :: left_index(:), standard_index(:)
+REAL, ALLOCATABLE :: data(:,:,:), data_left(:,:,:)
+INTEGER, EXTERNAL :: index1
+
+CHARACTER (LEN=256), PARAMETER :: PNAME='MAIN'
+CHARACTER (LEN=16), PARAMETER :: STANDARD_FILENAME='STD_PROFILE_IN'
+
+! Open standard BCON file generated from CMAQ
+
+OUT_DATA(:,:,:) = 0
+
+if (.not. open3( STANDARD_FILENAME, FSREAD3, PNAME ) ) then
+  write(6,*)
+  write(6,*) ' could not open file:', STANDARD_FILENAME
+  write(6,*)
+  stop
+end if
+
+if ( .not. desc3(STANDARD_FILENAME) ) then
+  write(6,*)
+  write(6,*) ' could not get file description:', STANDARD_FILENAME
+  write(6,*)
+  stop
+end if
+
+standard_nvar = nvars3d
+
+!------------------------------------------------------
+allocate (standard_vname(standard_nvar), standard_units(standard_nvar), standard_vdesc(standard_nvar), &
+          stat=stat)
+
+standard_vname = vname3d
+standard_units = units3d
+standard_vdesc = vdesc3d
+standard_jdate = sdate3d
+standard_jtime = stime3d
+
+allocate (standard_index(standard_nvar), stat=stat)
+
+!do i = 1, standard_nvar
+!  print *, "Check #1, ",  standard_vname(i)
+!end do
+
+!------------------------------------------------------
+allocate (geos_vname(NVARS), geos_units(NVARS), geos_vdesc(NVARS), stat=stat)
+
+geos_nvar = NVARS
+geos_vname = VNAME
+geos_units = UNITS
+geos_vdesc = VDESC
+
+!do i = 1, NVARS
+!  print *, "Check #2, ", geos_vname(i)
+!end do
+
+! Compare variable lists
+
+left_nvar = 0
+
+do v = 1, standard_nvar
+  standard_index(v) = index1(standard_vname(v), geos_nvar, geos_vname)
+  if (standard_index(v) .eq. 0) then
+    left_nvar = left_nvar + 1
+  end if
+end do
+
+allocate (left_vname(left_nvar), left_units(left_nvar), left_vdesc(left_nvar), left_index(left_nvar), &
+          stat=stat)
+
+i = 1
+do v = 1, standard_nvar
+  if (standard_index(v) .eq. 0) then
+    left_vname(i) = standard_vname(v)
+    left_units(i) = standard_units(v)
+    left_vdesc(i) = standard_vdesc(v)
+    i = i + 1
+  end if
+end do
+
+!do i = 1, left_nvar
+!  print *, "Check #3, ", left_vname(i)
+!end do
+
+! Allocate data storage
+if (ftype3d .eq. BNDARY3) then
+  allocate (data( 2*nthik3d * (ncols3d + nrows3d + 2*nthik3d), nlays3d, standard_nvar), stat=stat)
+  allocate (data_left( 2*nthik3d * (ncols3d + nrows3d + 2*nthik3d), nlays3d, left_nvar), stat=stat)
+end if
+
+if (stat .ne. 0) then
+  print *, ' Error allocating data'
+  stop
+end if
+
+! Setup up the output file
+if ( .not. read3(STANDARD_FILENAME, allvar3, allays3, standard_jdate, standard_jtime, data(:,:,:) ) ) then
+  print *, 'could not read from ', STANDARD_FILENAME
+  stop
+end if
+
+i = 1
+do v = 1, standard_nvar
+  if (standard_index(v) .eq. 0) then
+    data_left(:,:,i) = data(:,:,v)
+    i = i + 1
+  end if
+end do
+
+NVARS_LEFT = i - 1
+
+OUT_NAME(1:geos_nvar)  = geos_vname(1:geos_nvar)
+OUT_UNITS(1:geos_nvar) = geos_units(1:geos_nvar)
+OUT_DES(1:geos_nvar)   = geos_vdesc(1:geos_nvar)
+OUT_DATA(:,:,1:geos_nvar) = IN_DATA(:,:,1:geos_nvar)
+
+OUT_NAME(geos_nvar+1:geos_nvar+left_nvar)  = left_vname(1:left_nvar)
+OUT_UNITS(geos_nvar+1:geos_nvar+left_nvar) = left_units(1:left_nvar)
+OUT_DES(geos_nvar+1:geos_nvar+left_nvar)   = left_vdesc(1:left_nvar)
+OUT_DATA(:,:,geos_nvar+1:geos_nvar+left_nvar) = data_left(:,:,1:left_nvar)
+
+deallocate (left_vname)
+deallocate (left_units)
+deallocate (left_vdesc)
+deallocate (data_left)
+deallocate (data)
+
+RETURN
+END
